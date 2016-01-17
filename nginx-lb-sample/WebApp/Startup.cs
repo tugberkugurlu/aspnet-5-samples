f8dd6691132ce7a1cbd8da1617900bab43f3d59f
Tugberk Ugurlu <tugberk@outlook.com>
2016-01-17 11:11:21 +0000

    listing environment variables

--- nginx-lb-sample/WebApp/Startup.cs
+++ nginx-lb-sample/WebApp/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Text;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
@@ -23,7 +25,16 @@ namespace HelloWorldWeb
             app.Run(async ctx => 
             {
                 ctx.Response.StatusCode = 200;
-                await ctx.Response.WriteAsync($"Hello world from {GetMachineName()}!");
+                ctx.Response.Headers["Content-Type"] = "text/html";
+                StringBuilder builder = new StringBuilder();
+                builder.AppendLine($"<div>Hello from {GetMachineName()}! Here is my context:</div>");
+                builder.AppendLine("<hr/>");
+                foreach(DictionaryEntry envVar in System.Environment.GetEnvironmentVariables())
+                {
+                    builder.AppendLine(envVar.Key  + ":" + envVar.Value);
+                }
+                
+                await ctx.Response.WriteAsync($"<pre>{builder.ToString()}</pre>");
             });
         }
         
